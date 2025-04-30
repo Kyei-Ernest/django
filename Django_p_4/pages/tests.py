32593112d1bbe91dd0dae38bfef01c370a119236
Kyei-Ernest <Ernestkyei101@gmail.com>
2025-04-30 23:38:48 +0000

    2.1

--- Django_p_4/pages/tests.py
+++ Django_p_4/pages/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="admin", email="ernestkyei101@gmail.com",
+            password="admin123"
+        )
+        cls.post = Post.objects.create(
+            title="Ironman",
+            body="Everybody loved Ironman",
+            author=cls.user,
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "Ironman")
+
+        self.assertEqual(self.post.body, "Everybody loved Ironman")
+
+        self.assertEqual(self.post.author.username, "admin")
+
+        self.assertEqual(str(self.post), "Ironman")
+
+        self.assertEqual(self.post.get_absolute_url(),"/post/1/")
+
+    def test_url_exists_at_correct_location_listview(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exists_at_correct_location_detailview(self):
+        response = self.client.get("/post/1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Author:")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_post_detailview(self):
+        response = self.client.get(reverse("post_detail",kwargs={"pk": self.post.pk}))
+        no_response = self.client.get("/post/100000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "Ironman")
+        self.assertTemplateUsed(response, "post_detail.html")
