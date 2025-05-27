e3ca60044329482d8316a08985eb979499aa5646
Kyei-Ernest <Ernestkyei101@gmail.com>
2025-05-27 14:08:33 +0000

    tut3

--- tutorial2_serializers/snippets/serializers.py
+++ tutorial2_serializers/snippets/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
+"""class SnippetSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']"""
+
 
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
