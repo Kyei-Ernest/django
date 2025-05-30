01bb785b156944b267a7cc7c8adfe885bffbe382
Kyei-Ernest <Ernestkyei101@gmail.com>
2025-05-30 11:54:11 +0000

    4.6

--- tutorial2_serializers/snippets/serializers.py
+++ tutorial2_serializers/snippets/serializers.py
@@ -2,13 +2,26 @@ from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
-"""class SnippetSerialize(serializers.ModelSerializer):
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']"""
+        fields = ['url', 'id', 'highlight', 'owner', 'title', 'code',
+                  'linenos', 'language', 'style']
 
 
-class SnippetSerializer(serializers.Serializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
+
+
+"""class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
@@ -18,15 +31,15 @@ class SnippetSerializer(serializers.Serializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     def create(self, validated_data):
-        """
+        ""
         Create and return a new `Snippet` instance, given the validated data.
-        """
+        ""
         return Snippet.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        """
+        ""
         Update and return an existing `Snippet` instance, given the validated data.
-        """
+        ""
         instance.title = validated_data.get('title', instance.title)
         instance.code = validated_data.get('code', instance.code)
         instance.linenos = validated_data.get('linenos', instance.linenos)
@@ -34,11 +47,4 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
-
-
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'snippets']
+"""
--- tutorial2_serializers/snippets/urls.py
+++ tutorial2_serializers/snippets/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import SnippetDetail,SnippetList,UserList,UserDetail
 from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
 
 urlpatterns = [
-    path('snippets/', SnippetList.as_view()),
-    path('snippet/<int:pk>/', SnippetDetail.as_view()),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/',UserDetail.as_view())
+    path('', views.api_root),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippet/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/',views.UserDetail.as_view(), name='user-detail'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
